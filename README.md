52bb5f50f0ec28e48133fc2bfb7f8645323213a7
jjangga <jjangga@kookmin.ac.kr>
2018-11-10 22:38:30 +0900

    feature: #I1, #I2

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ saramin의 버그를 수정하고, 필요시 기능을 확장하기 위한 chrom
 * 단축키: `Ctrl+Shift+Z`(windows, linux), `Command+Shift+Z`(mac)
 * 설명: 2018.11.10 기준, saramin은 chrome 등 IE 이외의 브라우저에서 조회를 원하는 사람의 개인정보를 불러오지 못하는 버그가 존재한다. 이 기능은 해당 버그를 해결한다.
 * 기술적 접근: DOM 에서 `usemandb(candidateId)` 를 찾는다. `usemandb` 함수는 saramin 의 context 에 등록된 함수이므로 extension 에서는 직접 실행하지 못한다. 그러나 DOM 에는 접근이 가능하므로, extension에서는 `<body>` 에 보이지 않는 버튼(`onclick` 핸들러로 `usemandb(candidateId)`을 등록)을 추가하고 `click` 이벤트를 발생시켜 함수를 실행한다.
+
+## 클립보드에 이메일 복사 (**#I1**)
+* 단축키: `Ctrl+Shift+S`(windows, linux), `Command+Shift+S`(mac)
+* 기술적 접근: DOM에 `<textarea>` 를 만들고 value 를 이메일로 설정한다. 이후 해당 `<textarea>` 를 `select()` 하고 `document.execCommand('copy');` 를 실행한다.
+
+## 클립보드에 전화번호 복사 (**#I2**)
+* 단축키: `Ctrl+Shift+X`(windows, linux), `Command+Shift+X`(mac)
+* 기술적 접근: **#I1** 과 같다.
--- background.js
+++ background.js
@@ -14,36 +14,82 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
+function executeOnDOM(code) {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code});
+    });
+}
+
+// min (포함) 과 max (불포함) 사이의 임의 정수를 반환
+// Math.round() 를 사용하면 고르지 않은 분포를 얻게된다!
+function getRandomInt({
+                          min = 0,
+                          max = 1000000,
+                      } = {}) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function loadCandidateContact() {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        const color = 'black';
-        const uuid = 'this-is-the-unique-id-sjasldfhb';
-        const callOnOriginalContext = `changeColor(\\'${color}\\');`;
-        code = `
-        const newBtn = '<button style="display: none" id="${uuid}" onclick="${callOnOriginalContext}">click this</button>';
-        document.querySelector('body').innerHTML += newBtn;
-        document.querySelector('#${uuid}').click();`;
-
-        code = `
+    const uuid = `chrome-extension-saramin-loadCandidateContact-${getRandomInt()}`;
+    const code = `
         (function(){
             const command = document.querySelector('a[href^="javascript:useMandb("]').href.substr(11);
             const newBtn = '<button style="display: none" id="${uuid}" onclick="'+command+'">click this</button>';
             document.querySelector('body').innerHTML += newBtn;
             document.querySelector('#${uuid}').click();
         })();`;
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code});
-    });
+    executeOnDOM(code);
+    setTimeout(copyCandidateEmail, 100);
 }
-`
-hdsearch
-    hd5684415#
-`
-chrome.commands.onCommand.addListener(function (command) {
+
+function copy(parsingCode) {
+    const uuid = `chrome-extension-saramin-${getRandomInt()}`;
+    const code = `
+    (function(){
+        const data = ${parsingCode};
+        const newTextArea = '<textarea id="${uuid}">'+data+'</textarea>';
+        document.querySelector('body').innerHTML += newTextArea;
+        document.querySelector('#${uuid}').select();
+        document.execCommand('copy');
+    })();`;
+    console.log('copy:', code);
+    executeOnDOM(code);
+}
+
+function copyCandidateEmail() {
+    const parsingCode = `
+    (function(){
+        var element = document.querySelector('li.mail>span');
+        if(!element){
+            return '이메일없음';
+        }
+        return element.innerHTML;
+    })();`;
+
+    copy(parsingCode);
+}
+
+function copyCandidatePhone() {
+    const code = `
+    (function(){
+        var element = document.querySelector('a[resume-action="sendSMS"]')||document.querySelector('li.tel>span');
+        if(!element){
+            return '연락처없음';
+        }
+        return element.innerHTML;
+    })();`;
+    copy(code);
+}
+
+chrome.commands.onCommand.addListener((command) => {
     console.log('Command:', command);
     if (command == "candidate-contact-load") {
         loadCandidateContact();
+    } else if (command == "candidate-email-copy") {
+        copyCandidateEmail();
+    } else if (command == "candidate-phone-copy") {
+        copyCandidatePhone();
     }
 });
